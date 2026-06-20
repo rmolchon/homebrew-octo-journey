21358added03b8c56535ddc3674f1c0215b15e0a
rmolchon <robmolchon@gmail.com>
2026-06-20 13:29:38 -0400

    Fixed formula with alias and test

--- Formula/octo-journey.rb
+++ Formula/octo-journey.rb
@@ -17,20 +17,15 @@ class HomebrewOctoJourney < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    #system "./configure", "--disable-silent-rules", *std_configure_args
+
+    # here we are installing the shell script and giving it an alias of "tiny-sh-example"
+    bin.install "src/run.sh" => "octo-journey"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-octo-journey`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # this is a simple test that checks if the script is working as expected
+    system "#{bin}/octo-journey", "mvogelgesang"
+
   end
 end
