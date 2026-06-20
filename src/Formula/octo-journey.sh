249c4605f6ad5b4dd077823581bb6cb179aa73f8
rmolchon <robmolchon@gmail.com>
2026-06-20 13:19:39 -0400

    Added formula for brew

---
+++ src/Formula/octo-journey.sh
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewOctoJourney < Formula
+  desc "Test for homebrew"
+  homepage "https://github.com/rmolchon/homebrew-octo-journey"
+  url "https://github.com/rmolchon/homebrew-octo-journey/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "1b704708c53e16664b02c3acdf1f9a6d62576fb9193332f5e3163e0f35735a32"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-octo-journey`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
