712e4dc1ee2884fd6db6080aefb91a03a1a9851e
rmolchon <robmolchon@gmail.com>
2026-06-20 13:46:22 -0400

    updated formula with new version v0.0.3

--- Formula/octo-journey.rb
+++ Formula/octo-journey.rb
@@ -2,8 +2,8 @@
 class OctoJourney < Formula
   desc "Test for homebrew"
   homepage "https://github.com/rmolchon/homebrew-octo-journey"
-  url "https://github.com/rmolchon/homebrew-octo-journey/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "1b704708c53e16664b02c3acdf1f9a6d62576fb9193332f5e3163e0f35735a32"
+  url "https://github.com/rmolchon/homebrew-octo-journey/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "bb62bca6ed8defe6c401e8f545da0dc7f2a390f8fcaa72d90e866f7bb74821c8"
   license ""
 
   # depends_on "cmake" => :build
